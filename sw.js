89f0cb123e1020ac996b07c6da209b0e9788d810
Vincx12 <dylanboullard@gmail.com>
2026-06-01 17:49:11 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+// Service Worker Flappy Beer - cache pour fonctionnement hors-ligne
+const CACHE = 'flappy-beer-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-192.png',
+  './icons/maskable-512.png',
+  './screenshots/screen1.png',
+  './screenshots/screen2.png'
+];
+
+// Installation : on met en cache les fichiers du jeu
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+// Activation : on nettoie les anciens caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Requetes : on sert depuis le cache, sinon le reseau (et on met en cache)
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
